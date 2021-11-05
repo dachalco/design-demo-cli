a45b164ab7d65230298edc5e702d1f30fb9e9034
dachalco <david@chalco.io>
2021-11-05 16:12:31 -0700

    working usage of CLI v1.1.0-dev

--- main/main.c
+++ main/main.c
@@ -7,17 +7,99 @@
    CONDITIONS OF ANY KIND, either express or implied.
 */
 #include <stdio.h>
+#include <string.h>
 #include "sdkconfig.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/queue.h"
+#include "freertos/stream_buffer.h"
 
 #include "FreeRTOS_CLI.h"
 
 #include "driver/uart.h"
+#include "driver/gpio.h"
+#include "esp_intr_alloc.h"
+#include "soc/uart_struct.h"
+#include "soc/uart_reg.h"
+
+#include "esp_log.h"
+
+#define UART_CLI  ( UART_NUM_1 )
+#define RX_PIN    ( GPIO_NUM_18 )
+#define TX_PIN    ( GPIO_NUM_19 )
+#define BUF_SIZE  ( 512 )
+#define TAG       "UART-FEED"
+
+static QueueHandle_t xUartEvents = NULL;
+
+#ifdef WAS_ABLE_TO_GET_UART_ISR_WORKING
+static void IRAM_ATTR isr_uart_handler( void * arg )
+{
+    BaseType_t xShouldYield = pdFALSE;
+    uint16_t irq_status = UART1.int_st.val;
+    uint16_t n_read = UART1.status.rxfifo_cnt;
+    uint8_t byte = 0u;
+
+    /* Forward to CLI input stream */
+    for( int i=0; i<n_read; i++ )
+    {
+        byte = UART1.fifo.rw_byte;
+        FreeRTOS_CLIInStreamWriteFromISR( &byte, 1u, &xShouldYield );
+    }
+
+    uart_clear_intr_status( UART_CLI, UART_RXFIFO_FULL_INT_CLR | UART_RXFIFO_TOUT_INT_CLR );
+
+    if( xShouldYield )
+    {
+        taskYIELD();
+    }
+}
+#endif
 
 
-#define UART_CLI UART_NUM_0
-#define UART_CLI_
+static uint8_t xTmpBuffer[BUF_SIZE];
+static void prvUartEventHandlerTask( void *pvParameters )
+{
+    uart_event_t xEvent = { 0 };
+    size_t ulNWritten = 0;
+    size_t ulNRead = 0;
+
+    uint32_t ulInStreamPermit = FreeRTOS_CLIInStreamClaim( STREAM_WRITE, pdMS_TO_TICKS( 1000 ) );
+    uint32_t ulOutStreamPermit = FreeRTOS_CLIOutStreamClaim( STREAM_READ, pdMS_TO_TICKS( 1000 ) );
+    configASSERT( ulInStreamPermit & STREAM_WRITE );    
+    configASSERT( ulOutStreamPermit & STREAM_READ );
+
+    while(1)
+    {
+        /* Forward bytes to CLI instream */
+        if( xQueueReceive( xUartEvents, (void *)&xEvent, portMAX_DELAY ) )
+        {
+            memset( xTmpBuffer, 0u, sizeof(xTmpBuffer) );
+            
+            switch( xEvent.type )
+            {
+                case UART_DATA:
+                    uart_read_bytes( UART_CLI, xTmpBuffer, xEvent.size, portMAX_DELAY );
+                    ulNWritten = FreeRTOS_CLIInStreamWrite( xTmpBuffer, xEvent.size, pdMS_TO_TICKS( 1000 ) );
+                    ESP_LOGI( TAG, "UART --> Forwarded %d/%d B --> CLI In", ulNWritten, xEvent.size );
+                    break;
+                case UART_FIFO_OVF:
+                case UART_BUFFER_FULL:
+                    uart_flush_input( UART_CLI );
+                    xQueueReset( xUartEvents );
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        /* Forward CLI out stream bytes to UART */
+        ulNRead = FreeRTOS_CLIOutStreamRead( xTmpBuffer, sizeof(xTmpBuffer), pdMS_TO_TICKS( 1000 ) );
+        ulNWritten = uart_write_bytes( UART_CLI, (void *)xTmpBuffer, ulNRead );
+        ESP_LOGI( TAG, "CLI Out --> Forwarded %d/%d B --> UART", ulNWritten, ulNRead );
+    }
+
+}
 
 void app_main(void)
 {
@@ -28,11 +110,13 @@ void app_main(void)
     xUartConfig.parity = UART_PARITY_DISABLE;
     xUartConfig.stop_bits = UART_STOP_BITS_1;
     xUartConfig.flow_ctrl = UART_HW_FLOWCTRL_DISABLE;
-    uart_param_config( UART_CLI, &xUartConfig );
+    xUartConfig.source_clk = UART_SCLK_APB;
 
-    ESP_ERROR_CHECK( uart_set_pin( UART_CLI,  ));
+    ESP_ERROR_CHECK( uart_param_config( UART_CLI, &xUartConfig ) );
+    ESP_ERROR_CHECK( uart_driver_install( UART_CLI, BUF_SIZE * 2, BUF_SIZE * 2, 20, &xUartEvents, 0 ) );
+    ESP_ERROR_CHECK( uart_set_pin( UART_CLI, TX_PIN, RX_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE ) );
 
-    /* Get handles to CLI streams and tie to UART interrupt handler */
+    configASSERT( FreeRTOS_CLICreateTask() );
 
-    /* Scheduler is started just after app_main exit */
+    xTaskCreate( prvUartEventHandlerTask, "UART-EVENTS", configMINIMAL_STACK_SIZE + 2048, NULL, 12, NULL );
 }
